5d9a331bd61fee3cdbf3ac057552bcb7294b7db2
alexjungdev <90382701+PlaycideStudio@users.noreply.github.com>
2024-02-13 00:50:32 +0900

    feat: firebase function client proxy 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     reactStrictMode: false,
     swcMinify: true,
+    async rewrites() {
+        return [
+          {
+            source: '/api/auth',
+            destination: `${process.env.API_URL}`,
+          },
+        ];
+      },
 };
 
 export default nextConfig;
